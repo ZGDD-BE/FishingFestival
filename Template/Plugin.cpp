22686f469fe96a0a646d5706d77b604893d18d01
dofes <91889957+dofes@users.noreply.github.com>
2022-07-03 22:43:01 +0800

    Initial commit

---
+++ Template/Plugin.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include <EventAPI.h>
+#include <LoggerAPI.h>
+#include <MC/Level.hpp>
+#include <MC/BlockInstance.hpp>
+#include <MC/Block.hpp>
+#include <MC/BlockSource.hpp>
+#include <MC/Actor.hpp>
+#include <MC/Player.hpp>
+#include <MC/ItemStack.hpp>
+#include <LLAPI.h>
+
+static_assert(false, "Please set your plugin's name below and delete this line");
+Logger logger("PluginName");
+
+void PluginInit()
+{
+	static_assert(false, "Please modify your plugin registration details and delete this line");
+	LL::registerPlugin("PluginName", "Introduction", LL::Version(1, 0, 0));
+}
